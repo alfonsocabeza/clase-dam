46ef6e3ea4102c6b60c4cc9b151ede3efbaf8cc7
Alfonso Cabeza <alfonsocabeza34490@gmail.com>
2023-01-23 10:22:57 +0100

    commit

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -14,30 +14,30 @@ public class Alumno {
         nombre = nombreCompleto;
         numeroMatricula = numeroMatriculaAlumno;
         edad = edadAlumno;
-        
+
     }
-  
+
     /**
      * Devuelve el nombre completo del alumno
      */
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " años");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
@@ -51,29 +51,28 @@ public class Alumno {
             else {
                 if (nombre.length() < 3){
                     if (numeroMatricula.length() >= 4){
-                     github = nombre + numeroMatricula.substring(0, 4);
+                        github = nombre + numeroMatricula.substring(0, 4);
                     }
                     else{
-                      if (numeroMatricula.length() < 4){
-                          github =  nombre + numeroMatricula.substring(0, 4);
-                      }  
-                      
-                      }
-                    }
-                }
-            }  
-             else{
-                if (nombre.length() >= 3){
-                    if (numeroMatricula.length() < 4){
-                         github =  nombre.substring(0, 3) + numeroMatricula;
-                    }
-                    else{
-                        if (numeroMatricula.length() >= 4){
-                             github =  nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
-                        }                         
+                        if (numeroMatricula.length() < 4){
+                            github =  nombre + numeroMatricula.substring(0, 4);
+                        }  
                     }
                 }
             }
+        }  
+        else{
+            if (nombre.length() >= 3){
+                if (numeroMatricula.length() < 4){
+                    github =  nombre.substring(0, 3) + numeroMatricula;
+                }
+                else{
+                    if (numeroMatricula.length() >= 4){
+                        github =  nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+                    }                         
+                }
+            }
+        }
         return github;  
     }
 }
